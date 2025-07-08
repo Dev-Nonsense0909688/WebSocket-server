e77db0a1e88c7674de289424ffbb85e3f3886542
Dev-Nonsense0909688 <blackplasma001@gmail.com>
2025-07-08 11:40:05 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+// server.js
+const WebSocket = require('ws');
+const PORT = process.env.PORT || 3000;
+const server = new WebSocket.Server({ port: PORT });
+
+let clients = [];
+
+server.on('connection', ws => {
+    clients.push(ws);
+    ws.on('message', msg => {
+        for (let client of clients) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(msg.toString());
+            }
+        }
+    });
+
+    ws.on('close', () => {
+        clients = clients.filter(client => client !== ws);
+    });
+});
+
+console.log("WebSocket server running on port", PORT);
